88472b27d5b4cb8eb73a31b73a8e1bbe9c62a0f8
whibi1 <104696288+whibi1@users.noreply.github.com>
2023-03-29 01:12:05 +0300

    Update aralarında asal bulma.py

--- aralarında asal bulma.py
+++ aralarında asal bulma.py
@@ -1,10 +1,14 @@
 import math
+#Öncelikle math kütüphanesini çağırıyoruz.
+
 a=int(input(f"lütfen birinci sayıyı girin\n"))
 b=int(input(f"lütfen ikinci sayıyı girin\n"))
 c=math.gcd(a,b)
+#Daha sonrasında a,b değişkenlerinde  kullanıcıdan iki sayı isteyip bu değerleri c değişkeninde ebob metodu kullanarak karşılaştırıyoruz. 
 
 if math.gcd(a,b)==1:
         print(f"{a} ve {b} sayıları aralarında asaldır.")
 
 elif math.gcd(a,b)!=1:
         print(f"{a} ve {b} sayıları aralarında asal değildir. Ebobları {c} sayısıdır.")
+# eğer sayıların ebobu 1 çıkarsa sayıların asal olduğunu, başka bir sayı çıkarsa sayıların ebobunu yazdıran bir kod ekliyoruz.
